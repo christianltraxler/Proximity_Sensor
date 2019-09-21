5eb9a54bf64442370ded5f53aea5c37028bcbe98
christianltraxler <traxler.lc@gmail.com>
2019-09-21 10:52:32 -0400

    Update ProximityDetector.ino

--- ProximityDetector.ino
+++ ProximityDetector.ino
@@ -1,23 +1,33 @@
+//Include different libraries required
 #include "SR04.h"
 #include "pitches.h"
+//Define where the ultrasonic sensor connects 
+//to the Arduino
 #define TRIG_PIN 12
 #define ECHO_PIN 11
 SR04 sr04 = SR04(ECHO_PIN, TRIG_PIN);
 long a;
 
+//Setup the Arduino
 void setup() {
   Serial.begin(9600);
   delay(1000);
 }
 
 void loop() {
+  //Get and print the distance detected 
+  //between the sensor and the closest object
   a = sr04.Distance();
   Serial.print(a);
   Serial.println("cm");
 
+  //Check if the sensor detects an object 
+  //less than 1 meter away or greater than 
+  //2 meters away which can appear when an 
+  //object is detected and the sensor
+  //malfunctioned
   if (a < 100 || a > 200) {
-    int play = 0;
-    if (play == 0)
+    //Play the Jurassic Park theme
     tone(8, NOTE_C4, 2400);
     delay(2500);
     tone(8, NOTE_C4, 400);
@@ -124,8 +134,8 @@ void loop() {
     delay(500);
 
     tone(8, NOTE_C4, 3200);
-
-    ++play;
+    }
   }
+  //Delay the repeat of the program for 0.1 seconds
   delay(100);
 }
